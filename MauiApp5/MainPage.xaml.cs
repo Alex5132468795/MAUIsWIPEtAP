f472310b3a958a02400529c571164e53b8a75d6f
ilyaDavidov <ilya.davidov63@mail.ru>
2022-12-06 17:18:19 +0200

    qwe

---
+++ MauiApp5/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿namespace MauiApp5;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
+	{
+        OWO.Text = "==OWO";
+        OWO.TextColor = Color.FromRgb(122, 255, 122);
+        await OWO.TranslateTo(100, 0);
+        await OWO.TranslateTo(0, 0);
+    }
+	private async void SwipeGestureRecognizer_Swiped_2(object sender, SwipedEventArgs e)
+	{
+        await QWQ.TranslateTo(0,-50);
+        await QWQ.TranslateTo(0, 0);
+    }
+
+	private async void SwipeGestureRecognizer_Swiped_3(object sender, SwipedEventArgs e)
+	{
+		OWO.Text = "OWO==";
+		OWO.TextColor = Color.FromRgb(100,100,255);
+		await OWO.TranslateTo(-100,0);
+        await OWO.TranslateTo(0,0);
+    }
+
+	private async void SwipeGestureRecognizer_Swiped_4(object sender, SwipedEventArgs e)
+    {
+        await QWQ.TranslateTo(0,50);
+        await QWQ.TranslateTo(0, 0);
+    }
+
+	/*
+	private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+	{
+		var c = (sender as Frame);
+		if (c != null)
+		{
+			await c.RelRotateTo(45, 45);
+		}
+		else
+		{
+			var dc = (sender as VerticalStackLayout);
+			await dc.RelRotateTo(45, 45);
+		}
+    }
+	*/
+}
+
